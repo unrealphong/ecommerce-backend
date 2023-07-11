8a5de539de5d7fa899adeee8cb51a72441b5daca
hnrygram <phonghngle@gmail.com>
2023-07-12 01:19:59 +0700

    init middleware: helmet, morgan, compression

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-})
+require("dotenv").config({
+  path: process.env.NODE_ENV === "development" ? ".env.dev" : ".env",
+});
 
-const app = require('./src/app')
+const app = require("./src/app");
 
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+const server = app.listen(PORT, () => {
+  console.log(`WSV eCommerce start with ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log(`Exit Server Express`);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
-const express = require('express')
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
 
-const app = express()
+// init middleware
+app.use(morgan("dev"));
+// app.use(morgan('common'))
+// app.use(morgan("combined"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+app.use(helmet());
+app.use(compression());
+// init db
 
-module.exports = app
+// init router
+app.get("/", (_req, res) => {
+  const strCompress = "Hello le hong phong";
+  return res.status(200).json({
+    massage: "Welcome Fanpage",
+    metadata: strCompress.repeat(1000000),
+  });
+});
+// handle error
+
+module.exports = app;
