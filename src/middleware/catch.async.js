7b418dadbaf11eab9c4505db516dbb7be9dcfe5e
Le Hong Phong <hongphongle115@gmail.com>
2023-07-29 02:48:47 +0700

    refactor file catch.async.js

--- src/middleware/catch.async.js
+++ src/middleware/catch.async.js
@@ -1,13 +1,3 @@
-// module.exports = (fn) => (req, res, next) => {
-//   // Promise.resolve(fn(req, res, next)).catch((err) => console.log(err))
-//   // return next(err)
-//   return Promise.resolve(fn(req, res, next)).catch((err) => next(err))
-// }
-
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next)
-  }
+module.exports = (fn) => (req, res, next) => {
+  return Promise.resolve(fn(req, res, next)).catch((err) => next(err))
 }
-
-module.exports = catchAsync
